8d9d112702b0c0d3303ae1546c0716d1531e332a
tanv0205 <tanveerdace@gmail.com>
2021-12-01 15:21:48 +0530

    second commit

---
+++ Controllers/ProductsController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DockerApiEx2.Models;
+
+namespace DockerApiEx2.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private static List<Product> prods = new List<Product>();
+        public ProductsController()
+        {
+        }
+        [HttpPost]
+        public void Add(Product p)
+        {
+            prods.Add(p);
+        }
+        [HttpGet]
+        public IEnumerable<Product> GetAll()
+        {
+            return prods;
+        }
+        [HttpGet("{id}")]
+        public Product Get(int id)
+        {
+            return prods.Find(p => p.PId == id);
+        }
+        [HttpPut("{id}")]
+        public IEnumerable<Product> Update(int id)
+        {
+            Product p= prods.Find(p => p.PId == id);
+            p.PName = "brinjal";
+            return prods;
+        }
+        [HttpDelete("{id}")]
+        public void Remove(int id)
+        {
+            prods.RemoveAll(p => p.PId == id);
+        }
+    }
+}
---
+++ Models/Product.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DockerApiEx2.Models
+{
+    public class Product
+    {
+        public int PId { get; set; }
+        public string PName { get; set; }
+        public int PQty { get; set; }
+        public decimal PPrice { get; set; }
+    }
+}
